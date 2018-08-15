f94014408b1ace57502b4d7cd925b00fbd9edaf4
lbrucel <bruce.ledbetter@gmail.com>
2018-08-15 11:58:34 -0400

    fix key errors

--- client/components/RoomsList.js
+++ client/components/RoomsList.js
@@ -1,16 +1,17 @@
-import React from "react";
-import { Grid, Image } from "semantic-ui-react";
-import { connect } from "react-redux";
-import RoomSingleCard from "./roomSingleCard";
+import React from 'react';
+import {Grid, Image} from 'semantic-ui-react';
+import {connect} from 'react-redux';
+import RoomSingleCard from './roomSingleCard';
 
 export const RoomsList = props => {
+  console.log(props);
   return (
     <Grid columns="three" divided>
       <Grid.Row>
         {props.rooms.map(room => {
           return (
-            <Grid.Column>
-              <RoomSingleCard room={room} />
+            <Grid.Column key={room.id}>
+              <RoomSingleCard room={room} key={room.id} />
             </Grid.Column>
           );
         })}
