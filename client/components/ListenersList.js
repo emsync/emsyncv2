302426d7114712c14d15fa2e6b87b21e5c2e7192
Xanthe Coffman <xanthecoffman@gmail.com>
2018-08-17 15:56:03 -0400

    a lil more

--- client/components/ListenersList.js
+++ client/components/ListenersList.js
@@ -5,26 +5,32 @@ import {ListenerElement} from './ListenerElement';
 //props that should be passed here should be the current rooms listeners array
 const ListenersList = props => (
   <div>
-    {props.listeners.length ? (
-      <Card>
-        <Card.Content>
-          <Card.Header>Listeners</Card.Header>
-        </Card.Content>
-        <Card.Content>
-          <Feed>
+    <Card>
+      <Card.Content>
+        <Card.Header>Listeners</Card.Header>
+      </Card.Content>
+      <Card.Content>
+        <Feed>
+          {props.listeners.length ? (
+            props.listeners.map(listener => {
+              return (
+                <Card.Content>
+                  <Feed.Event>
+                    <Feed.Content>
+                      <ListenerElement key={listener} listener={listener} />
+                    </Feed.Content>
+                  </Feed.Event>
+                </Card.Content>
+              );
+            })
+          ) : (
             <Feed.Event>
-              <Feed.Content>
-                {props.listeners.map(listener => {
-                  return <ListenerElement key={listener} listener={listener} />;
-                })}
-              </Feed.Content>
+              <Feed.Content>You're the only listener!</Feed.Content>
             </Feed.Event>
-          </Feed>
-        </Card.Content>
-      </Card>
-    ) : (
-      <p>You're the only listener!</p>
-    )}
+          )}
+        </Feed>
+      </Card.Content>
+    </Card>
   </div>
 );
 
